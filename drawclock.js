8e9639f20c92d696cfbffb7a915aaa2e3a0ecfb6
Brandon <brx021@gmail.com>
2020-08-07 18:34:19 -0700

    Brandon's update

--- drawclock.js
+++ drawclock.js
@@ -1,19 +1,11 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-ctx.font = "75px sans-serif";
-ctx.fillStyle = "white";
 setInterval(drawTime, 1000);
 
 function drawTime() {
-    ctx.fillStyle = "teal";
-    ctx.fillRect(0, 0, 447, 200);
-
-    ctx.fillStyle = "white";
     var now = new Date();
     var hour = cleanUpHour(now.getHours());
     var minute = cleanUp(now.getMinutes());
     var second = cleanUp(now.getSeconds());
-    ctx.fillText(hour + " : " + minute + " : " + second, 32, 120);
+    document.getElementById("clock").innerHTML = hour + " : " + minute + " : " + second;
 }
 
 function cleanUp(i){
